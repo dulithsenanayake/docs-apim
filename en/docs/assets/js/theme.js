c8b3110b854c01ec32c0e2f38d1e6adb4df0178e
Ruthryi Kulasekaran <ruthryi@wso2.com>
2023-08-17 17:48:49 +0530

    Test APIM

--- en/docs/assets/js/theme.js
+++ en/docs/assets/js/theme.js
@@ -19,23 +19,6 @@
 /*
 * Handle opening external links in a new tab
 */
-/* 
- * Initialize highlightjs 
- */
-hljs.initHighlightingOnLoad();
-(function() {
-    if(document.querySelector('.tab-selector')){
-        document.querySelector('.tab-selector').addEventListener('click', function(e) {
-            // Show hide tab content next to the clicked tab
-            var tabContentToShow = e.target.nextElementSibling;
-            if(tabContentToShow.style.display === 'none') {
-                tabContentToShow.style.display = 'block';
-            } else {
-                tabContentToShow.style.display = 'none';
-            }
-        });
-    }
-})();
 
 (function() {
     var links = document.links;
@@ -110,7 +93,7 @@ for (var i = 0; i < dropdowns.length; i++) {
  * Reading versions
  */
 var pageHeader = document.getElementById('page-header');
-var docSetLang = (pageHeader && pageHeader.getAttribute('data-lang')) || 'en';
+var docSetLang = pageHeader.getAttribute('data-lang');
 
 (window.location.pathname.split('/')[1] !== docSetLang) ? 
     docSetLang = '' :
@@ -217,6 +200,11 @@ request.onerror = function() {
 };
 
 request.send();
+
+/* 
+ * Initialize highlightjs 
+ */
+hljs.initHighlightingOnLoad();
 
 /*
  * Handle TOC toggle
